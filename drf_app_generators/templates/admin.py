53298af16e5109870c0b7bbd4fa648cc1884f6e8
Huy Tran <huy.tranquoc@asnet.com.vn>
2020-03-05 09:36:39 +0700

    Update admin template

--- drf_app_generators/templates/admin.py
+++ drf_app_generators/templates/admin.py
@@ -1,4 +1,16 @@
 __all__ = ['ADMIN_VIEW']
 
-ADMIN_VIEW = """from core import admin
-"""
+ADMIN_VIEW = """from django.contrib import admin
+from core.admin import BaseModelAdmin
+
+from {{ app }}.models import ({% for model in models %}
+    {{ model }},{% endfor %}
+)
+{% for model in models %}
+
+# =============================================================================
+# {{ model }}
+# =============================================================================
+class {{ model }}Admin(BaseModelAdmin):
+    pass
+{% endfor %}"""
