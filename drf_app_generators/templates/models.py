b0ac1bc47cb772a989dacda4c5cf73badeef0300
Huy Tran <huy.tranquoc@asnet.com.vn>
2020-03-06 16:36:45 +0700

    Tune up code style

--- drf_app_generators/templates/models.py
+++ drf_app_generators/templates/models.py
@@ -1,6 +1,7 @@
 __all__ = ['MODEL_VIEW']
 
 MODEL_VIEW = """from django.db import models
+
 from drf_core.models import TimeStampedModel, QuerySet
 from drf_core import fields
 {% for model in models %}
